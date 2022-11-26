88aad6cd00f5a892c74cc5894dfa5758d932c9c5
Sid <sid@dockvine.com>
2022-11-26 17:24:21 +0700

    test build process 2

--- src/components/Tweet/Tweet.js
+++ src/components/Tweet/Tweet.js
@@ -12,7 +12,7 @@ import styles from "./styles";
 class Tweet extends React.Component {
   constructor(props) {
     super(props);
-    console.log("tweet 1");
+    console.log("tweet 2");
     this.toggleModal = this.toggleModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
     this.state = {
